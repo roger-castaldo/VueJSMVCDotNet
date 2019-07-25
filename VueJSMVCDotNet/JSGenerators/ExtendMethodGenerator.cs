0492bbc61e541a86d749a5acad244725a4d62a07
Roger Castaldo <roger_castaldo@lakeheadschools.ca>
2019-07-25 10:36:38 -0400

    validation
    corrected bug wrt extension calls to extend the defined objects.
    Added in model validation basics to specify a required field in the update and save calls through the computed valid of isValid

--- VueJSMVCDotNet/JSGenerators/ExtendMethodGenerator.cs
+++ VueJSMVCDotNet/JSGenerators/ExtendMethodGenerator.cs
@@ -9,13 +9,13 @@ namespace Org.Reddragonit.VueJSMVCDotNet.JSGenerators
     {
         public void GeneratorJS(ref WrappedStringBuilder builder, bool minimize, Type modelType)
         {
-            builder.AppendLine(string.Format(@" var ext = function(clazz,data){{
-        var ret = (clazz._extend==undefined ? clazz.extend(data) : clazz._extend(data));
-        ret._extend=clazz.extend;
+            builder.AppendLine(string.Format(@"     var ext = function(clazz,data){{
+        var ret = $.extend((clazz._extend==undefined ? clazz.extend(data) : clazz._extend(data)),{1});
+        ret._extend=(clazz._extend==undefined ? clazz.extend : clazz._extend);
         ret.extend = function(data){{
             return ext(this,data);
         }};
-        return $.extend(ret,{1});
+        return ret;
     }};
     App.Models.{0}=ext(App.Models.{0},{{}});", new object[] { modelType.Name,Constants.STATICS_VARAIBLE}));
         }
