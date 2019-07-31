860a9de894c2088ff1e60dac7491e1fddf9fd71b
roger-castaldo <roger.castaldo@gmail.com>
2019-07-31 09:04:40 -0400

    nuget packaging
    implemented nuget packagin and deployed version 1.0

--- VueJSMVCDotNet/obj/Debug/netcoreapp2.1/VueJSMVCDotNet.AssemblyInfo.cs
+++ VueJSMVCDotNet/obj/Debug/netcoreapp2.1/VueJSMVCDotNet.AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("VueJSMVCDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute(@"A DotNet to VueJS interface library.  This is used to generate model(mvc) style Vues designed to allow an integrated interface between the javascript and backend for data manipulation and obtainment.  It does require that you include vuejs,jquery and underscorejs in your project as the code uses it.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VueJSMVCDotNet")]
